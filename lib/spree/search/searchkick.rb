d9276b32ea0bcb52aac9ff0fa03f2543b45d8600
sechix <sechix@gmail.com>
2018-04-05 19:03:12 +0200

    error

--- lib/spree/search/searchkick.rb
+++ lib/spree/search/searchkick.rb
@@ -13,7 +13,7 @@ module Spree
           keyword_query,
           where: where_query,
           aggs: aggregations,
-          fields: ["name", "brand", "search_by"],
+          fields: ["name", "brand", "name_and_brand_taxons_descripction"],
           includes: search_includes,
           smart_aggs: true,
           order: sorted,

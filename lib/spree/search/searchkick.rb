39bf86e21639f28eaf6bd8aeeba005147be2936f
sechix <sechix@gmail.com>
2018-04-05 16:13:12 +0200

    add mix search brand name taxons

--- lib/spree/search/searchkick.rb
+++ lib/spree/search/searchkick.rb
@@ -13,7 +13,7 @@ module Spree
           keyword_query,
           where: where_query,
           aggs: aggregations,
-          fields: ["name", "brand", "description", "taxon_names"],
+          fields: ["name", "brand", "name_and_brand_taxons_descripction", "taxon_names", "description"],
           includes: search_includes,
           smart_aggs: true,
           order: sorted,
